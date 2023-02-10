8403425527813a4450e20078c591e3822e12b556
Tina Yang <Tinayangers@gmail.com>
2023-02-10 14:11:41 -0500

    Added header and nav components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 import PatientList from './components/PatientList'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import Header from './components/Header'
 
 function App() {
   return (
+    // nav bar for home (PatientList) - medication list if I have time
+    // add search bar
     <div className="App">
+      <Header title='Med Minder' />
       <PatientList />
     </div>
   );
