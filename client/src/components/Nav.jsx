9545fb0c04ef7d213f36e5c39551f5ae77d26cb7
Tina Yang <Tinayangers@gmail.com>
2023-02-14 13:53:03 -0500

    Added Nav functionality

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,14 @@
+import { NavLink } from 'react-router-dom'
+
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav className='navbar'>
+            {/* <h4>Patients List</h4> */}
+            <div>
+                <NavLink to='/'>Patients List</NavLink>
+            </div>
+        </nav>
+
     )
 }
 export default Nav
