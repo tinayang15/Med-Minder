8b0651e7cfe758a03d0c47b1505d54a2f8bdb5e4
Tina Yang <Tinayangers@gmail.com>
2023-02-16 15:04:38 -0500

    Updated seeds

--- seed/patientsMedications.js
+++ seed/patientsMedications.js
@@ -13,12 +13,46 @@ const main = async () => {
         name: 'John Doe'
     })
     patient2.save()
+    const patient3 = await new Patient({
+        name: 'Jonny Doe'
+    })
+    patient3.save()
+    const patient4 = await new Patient({
+        name: 'Janny Doe'
+    })
+    patient4.save()
+    const patient5 = await new Patient({
+        name: 'Mary Doe'
+    })
+    patient5.save()
+    const patient6 = await new Patient({
+        name: 'Matthew Doe'
+    })
+    patient6.save()
 
 
     const medications = [
-        { Name: 'Medication One', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient1._id },
-        { Name: 'Medication Two', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient1._id },
-        { Name: 'Medication Three', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient2._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient1._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient1._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient1._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient2._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient2._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient2._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient3._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient3._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient3._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient4._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient4._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient4._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient5._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient5._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient5._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient6._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient6._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient6._id },
+        { Name: 'Naproxen', Dose: '10mg', Prescription: 'Take 1 tablet in the morning, afternoon and evening.', patientId: patient1._id },
+        { Name: 'Cyclobenzaprine', Dose: '12mg', Prescription: 'Take 1 tablet before bedtime or as needed for muscle spasms', patientId: patient2._id },
+        { Name: 'Amoxicillin', Dose: '3mg/2.5mL', Prescription: 'Take 5ml in the morning, afternoon and evening.', patientId: patient3._id },
     ]
     await Medication.insertMany(medications)
     console.log("Added medications!")
