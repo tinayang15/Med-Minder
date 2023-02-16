2a7754f1e2a1e60b7b9c930c387210c4c239c0b1
Tina Yang <Tinayangers@gmail.com>
2023-02-16 10:20:04 -0500

    Added heroku to package.json and server.js

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ app.use(logger('dev'))
 
 app.use('/home', routes);
 
+app.use(express.static(`${__dirname}/client/build`))
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
 
 app.listen(PORT, () =>
     console.log(`Listening on port: ${PORT}`))
